f78f70e000999cf1de6de5443e82794dd88d6ae7
Kalindu De Costa <kalinduk.decosta@gmail.com>
2023-03-26 19:43:33 -0400

    Use packer to automate vm template for ubuntu-server

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+    https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+    sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+sudo apt-get update
+sudo apt-get --dist upgrade
+
+sudo apt-get install python3
+sudo apt-get install golang-go
+sudo apt-get install terraform
+sudo apt install packer
+
+
+scripts/install-base.sh
---
+++ setup.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+echo ""
+echo "Installing core dependencies..."
+pip3 install -r requirements.txt
+
+echo ""
+echo "Installing docs dependencies..."
+pip3 install -r requirements-docs.txt
+
